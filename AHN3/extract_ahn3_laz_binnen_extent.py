67f8fc44f55dcf35ceffe41bac88d765202cdeb2
Jelmer Oosthoek <jhpoosthoek@gmail.com>
2016-03-13 13:51:41 +0100

    updates

---
+++ AHN3/extract_ahn3_laz_binnen_extent.py
@@ -0,0 +1,55 @@
+##extentsf=vector
+##lastools_folder=folder
+##laz_folder=folder
+##extent_buffer=number 0
+
+import os, sys
+from osgeo import ogr
+from qgis.core import *
+from PyQt4.QtCore import *
+
+output = os.path.join(laz_folder, os.path.basename(extentsf)[:-4] + "_xyzc.csv")
+
+listfile = os.path.join(laz_folder, "list.txt")
+os.chdir(laz_folder)
+
+ds = ogr.Open(extentsf)
+lyr = ds.GetLayer(0)
+extent = lyr.GetExtent()
+
+xmin = extent[0] - extent_buffer
+xmax = extent[1] + extent_buffer
+ymin = extent[2] - extent_buffer
+ymax = extent[3] + extent_buffer
+
+os.system("dir/b *.LAZ>list.txt")
+command = '%s -lof list.txt -inside %s %s %s %s -otxt -oparse xyzc' % (os.path.join(lastools_folder, "laszip"), xmin, ymin, xmax, ymax)
+os.system(command)
+
+f = open(listfile, "r")
+out = open(output, "w")
+out.write("X,Y,Z,CLASS\n")
+for filename in f:
+    filename = filename.strip()[:-4] + ".txt"
+    print filename
+
+    file = open(filename, "r")
+    for line in file:
+        line = line.strip().split(" ")
+        try:
+            x = float(line[0])
+            y = float(line[1])
+            z = float(line[2])
+            c = line[3]
+            WRITE = 1
+            if x > xmax:WRITE = 0
+            if x < xmin:WRITE = 0
+            if y > ymax:WRITE = 0
+            if y < ymin:WRITE = 0
+            if WRITE == 1:
+                out.write("%s,%s,%s,%s\n" % (x, y, z, c))
+        except:
+            pass
+    file.close()
+out.close()
+f.close()
---
+++ AHN3/make_ahn3_laz.py
@@ -0,0 +1,50 @@
+# https://pythonadventures.wordpress.com/2010/10/17/check-if-url-exists/
+# http://www.ahn.nl/pagina/open-data.html > http://www.ahn.nl/binaries/content/assets/hwh---ahn/common/ahn_units.zip
+# https://www.pdok.nl/nl/ahn3-downloads
+# https://github.com/jhpoosthoek/Python-shapefile-class
+
+from shapefile import shapefile
+import httplib
+import urlparse
+ 
+def get_server_status_code(url):
+    """
+    Download just the header of a URL and
+    return the server's status code.
+    """
+    # http://stackoverflow.com/questions/1140661
+    host, path = urlparse.urlparse(url)[1:3]    # elems [1] and [2]
+    try:
+        conn = httplib.HTTPConnection(host)
+        conn.request('HEAD', path)
+        return conn.getresponse().status
+    except StandardError:
+        return None
+ 
+def check_url(url):
+    """
+    Check if a URL exists without downloading the whole file.
+    We only check the URL header.
+    """
+    # see also http://stackoverflow.com/questions/2924422
+    good_codes = [httplib.OK, httplib.FOUND, httplib.MOVED_PERMANENTLY]
+    return get_server_status_code(url) in good_codes
+
+shpfile = "ahn_units.shp"
+inshp = shapefile("read", shpfile)
+fieldslist = []
+for line in inshp.fieldslist:
+    fieldslist.append(line)
+fieldslist.append(['DOWNLOAD', 4, 100])
+    
+outshp = shapefile("write", shpfile[:-4] + "_new.shp", inshp.type, fieldslist, inshp.projection)
+for feat in inshp.features:
+    attr_dict = inshp.attr_dict(feat)
+    name = attr_dict["UNIT"].upper()
+    url = "https://geodata.nationaalgeoregister.nl/ahn3/extract/ahn3_laz/C_%s.LAZ" % (name)
+    attr_dict["DOWNLOAD"] = url
+    if check_url(url):
+        print url
+        outshp.createfeat(feat, attr_dict)
+inshp.finish()
+outshp.finish()
